3a03c9b9e6e9abaf8dcf8134085ff8cf2d377c3d
Matheus Vinicius <mviniciuscardoso546@gmail.com>
2022-01-22 14:31:46 -0300

    animando seções a partir do scroll

--- script.js
+++ script.js
@@ -63,3 +63,26 @@ function initScrollSuave() {
   })
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections.length) {
+    const halfWindow = window.innerHeight * 0.65;
+
+    function animateScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - halfWindow) < 0;
+        if(isSectionVisible) {
+          section.classList.add('ativo')
+        }
+      })
+    }
+
+    animateScroll();
+
+    window.addEventListener('scroll', animateScroll)
+  }
+}
+initAnimacaoScroll();
